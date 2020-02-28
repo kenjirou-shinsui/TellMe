3251f3b88db603ff109d93ce4605e97b5a75bae3
kenjirou-shinsui <kenjirou.shinsui.0221@gmail.com>
2020-02-28 00:05:28 +0000

    [Edit]views/homes/aboutの画像表示に対する記述変更

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -7,7 +7,7 @@ class Admin::QuestionsController < ApplicationController
     end
 
     def index
-        @questions = Question.page(params[:page]).per(10).reverse_order
+        @questions = Question.page(params[:page]).per(10)
     end
 
     def edit
